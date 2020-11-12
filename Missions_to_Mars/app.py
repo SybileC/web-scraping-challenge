7c64884cac5841ab2a3afbc31afa1e2275572784
SybileC <sybile.cherenfant@gmail.com>
2020-11-11 19:38:07 -0500

    Created route

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -5,7 +5,10 @@ import scrape_mars
 app = Flask(__name__)
 
 @app.route("/scrape")
-def scraper():
-    mars_info = scrape_mars.scrape()
+def scrape():
+    mars_info = scrape_mars.scrape_info()
+    return render_template("index.html", mars = mars_info)
 
 
+if __name__ == "__main__":
+    app.run(debug=True)
