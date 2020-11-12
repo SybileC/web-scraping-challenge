c112a19d87c247a8fd2ae5f1a1969426b1ebc1be
SybileC <sybile.cherenfant@gmail.com>
2020-11-11 22:11:00 -0500

    Edited HTML file

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -4,10 +4,25 @@ import scrape_mars
 
 app = Flask(__name__)
 
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
 @app.route("/scrape")
 def scrape():
     mars_info = scrape_mars.scrape_info()
-    return render_template("index.html", mars = mars_info)
+
+    mongo.db.collection.update({}, mars_info, upsert=True)
+
+    print(mars_info)
+
+    #return render_template("index.html", mars = mars_info)
+
+
+@app.route("/")
+def index():
+    mars_data = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", astrology=mars_data)
 
 
 if __name__ == "__main__":
